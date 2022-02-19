0ba4ce690bd49375829a32aa63810140046b9e32
Daisuke Sato <tiryoh@gmail.com>
2022-02-19 17:16:56 +0900

    Add option to use D415/D435/D435i

--- 02-realsense_camera/README.md
+++ 02-realsense_camera/README.md
@@ -19,3 +19,12 @@ Option explained:
 * --network host To read all topics outside the container need to share the same network to the host
 * -v is the mounting directory
 *  --privileged Allow access to host
+
+## Advanced options
+
+To launch with other RealSense like D415 and D435, add an `realsense_type` argument.
+
+```
+docker run --runtime nvidia --network host --privileged rbonghi/isaac-ros-tutorial:realsense-camera \
+ros2 launch isaac_ros_realsense isaac_ros_realsense.launch.py realsense_type:=d415
+```
--- 02-realsense_camera/isaac_ros_realsense/launch/isaac_ros_realsense.launch.py
+++ 02-realsense_camera/isaac_ros_realsense/launch/isaac_ros_realsense.launch.py
@@ -19,31 +19,27 @@
 # DEALINGS IN THE SOFTWARE.
 
 import os
-import launch
-from launch_ros.actions import ComposableNodeContainer, Node
-from launch_ros.descriptions import ComposableNode
+from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
-
+from launch.actions import DeclareLaunchArgument
+from launch.actions import OpaqueFunction
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.actions import Node
+from launch_ros.descriptions import ComposableNode
 import xacro
-import tempfile
-
-def to_urdf(xacro_path, parameters=None):
-    """Convert the given xacro file to URDF file.
-    * xacro_path -- the path to the xacro file
-    * parameters -- to be used when xacro file is parsed.
-    """
-    urdf_path = tempfile.mktemp(prefix="%s_" % os.path.basename(xacro_path))
-
-    # open and process file
-    doc = xacro.process_file(xacro_path, mappings=parameters)
-    # open the output file
-    out = xacro.open_output(urdf_path)
-    out.write(doc.toprettyxml(indent='  '))
-
-    return urdf_path
 
 def generate_launch_description():
     """Launch file which brings up visual odometry node configured for RealSense."""
+
+    realsense_type = LaunchConfiguration('realsense_type')
+
+    declare_realsense_type_cmd = DeclareLaunchArgument(
+        name='realsense_type',
+        default_value='d435i',
+        description='RealSense type: d415, d435, d435i')
+
     realsense_camera_node = Node(
         package='realsense2_camera',
         executable='realsense2_camera_node',
@@ -59,53 +55,69 @@ def generate_launch_description():
         }],
         )
 
-    visual_odometry_node = ComposableNode(
-        name='visual_odometry_node',
-        package='isaac_ros_visual_odometry',
-        namespace='camera',
-        plugin='isaac_ros::visual_odometry::VisualOdometryNode',
-        parameters=[{
-                    'enable_rectified_pose': False,
-                    'denoise_input_images': False,
-                    'rectified_images': True,
-                    'enable_debug_mode': False,
-                    'enable_imu': True,
-                    'debug_dump_path': '/tmp/elbrus',
-                    'left_camera_frame': 'camera_infra1_frame',
-                    'right_camera_frame': 'camera_infra2_frame',
-                    'fixed_frame': 'odom',
-                    'imu_frame': 'camera_imu_optical_frame',
-                    'current_smooth_frame': 'base_link',
-                    'current_rectified_frame': 'base_link_rect'
-                    }],
-        remappings=[('stereo_camera/left/image', 'infra1/image_rect_raw'),
-                    ('stereo_camera/left/camera_info', 'infra1/camera_info'),
-                    ('stereo_camera/right/image', 'infra2/image_rect_raw'),
-                    ('stereo_camera/right/camera_info', 'infra2/camera_info'),
-                    ('visual_odometry/imu', 'imu')]
-    )
-
-    visual_odometry_launch_container = ComposableNodeContainer(
-        name='visual_odometry_launch_container',
-        namespace='camera',
-        package='rclcpp_components',
-        executable='component_container',
-        composable_node_descriptions=[
-            visual_odometry_node
-        ],
-        output='screen'
-    )
-
-    xacro_path = os.path.join(get_package_share_directory('realsense2_description'), 'urdf', 'test_d435i_camera.urdf.xacro')
-    urdf = to_urdf(xacro_path, {'use_nominal_extrinsics' : 'true', 'add_plug' : 'true'})
-
-    model_node = Node(
-        name='model_node',
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        namespace='',
-        output='screen',
-        arguments = [urdf]
+    # Use OpaqueFunction to parse argument
+    # ref: https://github.com/rt-net/raspimouse_ros2_examples/blob/foxy-devel/launch/teleop_joy.launch.py
+    def func_launch_visual_odometry_container(context):
+        visual_odometry_node = ComposableNode(
+            name='visual_odometry_node',
+            package='isaac_ros_visual_odometry',
+            namespace='camera',
+            plugin='isaac_ros::visual_odometry::VisualOdometryNode',
+            parameters=[{
+                'enable_rectified_pose': False,
+                'denoise_input_images': False,
+                'rectified_images': True,
+                'enable_debug_mode': False,
+                'enable_imu': True if context.launch_configurations['realsense_type'] == "d435i" else False,
+                'debug_dump_path': '/tmp/elbrus',
+                'left_camera_frame': 'camera_infra1_frame',
+                'right_camera_frame': 'camera_infra2_frame',
+                'fixed_frame': 'odom',
+                'imu_frame': 'camera_imu_optical_frame',
+                'current_smooth_frame': 'base_link',
+                'current_rectified_frame': 'base_link_rect'
+            }],
+            remappings=[('stereo_camera/left/image', 'infra1/image_rect_raw'),
+                        ('stereo_camera/left/camera_info', 'infra1/camera_info'),
+                        ('stereo_camera/right/image', 'infra2/image_rect_raw'),
+                        ('stereo_camera/right/camera_info', 'infra2/camera_info'),
+                        ('visual_odometry/imu', 'imu')]
         )
+        return [
+            ComposableNodeContainer(
+                name='visual_odometry_launch_container',
+                namespace='camera',
+                package='rclcpp_components',
+                executable='component_container',
+                composable_node_descriptions=[
+                    visual_odometry_node
+                ],
+                output='screen'
+            )
+        ]
 
-    return launch.LaunchDescription([visual_odometry_launch_container, realsense_camera_node, model_node])
+    visual_odometry_launch_container = OpaqueFunction(function=func_launch_visual_odometry_container)
+
+    def func_launch_model_node(context):
+        xacro_path = os.path.join(get_package_share_directory('realsense2_description'), 'urdf', 'test_{}_camera.urdf.xacro'.format(context.launch_configurations['realsense_type']))
+        parameters = {'use_nominal_extrinsics' : 'true', 'add_plug' : 'true'}
+        robot_desc = xacro.process_file(xacro_path, mappings=parameters).toprettyxml(indent='  ')
+        return [Node(
+            name='model_node',
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            namespace='',
+            output='screen',
+            parameters=[{'robot_description': robot_desc}]
+        )]
+
+    model_node = OpaqueFunction(function=func_launch_model_node)
+
+    ld = LaunchDescription()
+    
+    ld.add_action(declare_realsense_type_cmd)
+    ld.add_action(visual_odometry_launch_container)
+    ld.add_action(realsense_camera_node)
+    ld.add_action(model_node)
+
+    return ld
